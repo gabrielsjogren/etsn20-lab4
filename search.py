2929b728d0529f5e95702287c95a51537af17ee2
gabrielsjogren <gabbe.gs@gmail.com>
2024-12-05 10:23:20 +0100

    (fix) requested changes - error handling, method rename

--- search.py
+++ search.py
@@ -1,13 +1,18 @@
 import sys
 
-def main(word_to_search, file_to_search):
-    with open(file_to_search, "r") as file:
-        for line in file:
-            if word_to_search in line:
-                print(line)
+def search(word_to_search, file_to_search):
+    try:
+        with open(file_to_search, "r") as file:
+            for line in file:
+                if word_to_search in line:
+                    print(line)
+    except FileNotFoundError:
+        print(f"File {file_to_search} not found. please make sure the file exists. ")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     # Parse args
     word_to_search = sys.argv[1]
     file_to_search = sys.argv[2]
-    main(word_to_search, file_to_search)
+    search(word_to_search, file_to_search)
